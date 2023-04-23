f105c6e75773b4da8a8e56a73105da753ac371d2
henrikopsahl <henrikopsahl@hotmail.com>
2023-04-23 13:34:19 +0200

    added store

---
+++ JS/shop-details.js
@@ -0,0 +1,34 @@
+async function getProductDetails(productId) {
+    const url = `https://www.jagaranti.icu/wp-json/wc/store/products/${productId}?_embed`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get('id');
+  
+  getProductDetails(productId).then(function(product) {
+    console.log(product);
+    displayProductDetails(product);
+  });
+  
+  function displayProductDetails(product) {
+    const productContainer = document.querySelector(".product-container");
+    let html = "";
+  
+    html += `
+      <h1>${product.name}</h1>
+      <img src="${product.images[0].src}" alt="${product.name}">
+      <p>${product.short_description}</p>
+      <p>Price: ${product.prices.price.currency_decimal_seperator}</p>
+    `;
+  
+    productContainer.innerHTML = html;
+  }
+  
---
+++ JS/shop.js
@@ -0,0 +1,200 @@
+
+  /*const url = "https://www.jagaranti.icu/wp-json/wc/store/products?_embed";
+
+  async function getProducts() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  getProducts().then(function(products) {
+    console.log(products);
+    displayProducts(products);
+  });
+  
+  function displayProducts(products) {
+    const productContainer = document.querySelector(".product-container");
+    let html = "";
+  
+    products.forEach(function(product) {
+      let imageUrl = "";
+      let imageAlt = "No image available";
+      if (product.images && product.images.length > 0) {
+        imageUrl = product.images[0].src;
+        imageAlt = product.name;
+      }
+  
+      const addToCart = product.add_to_cart;
+      const addToCartText = addToCart ? addToCart.text : "Add to cart";
+      const addToCartUrl = addToCart ? addToCart.url : "";
+      const addToCartButton = `
+        <a href="${addToCartUrl}" class="add-to-cart">${addToCartText}</a>
+      `;
+  
+      const shortDescription = product.short_description;
+      const shortDescriptionText = shortDescription ? shortDescription : "During the mid-1980s, the rap group NWA rises from the harsh streets of Compton, located in Los Angeles, California. Their music and lyrics, which depict life in the hood, lead to a revolution in Hip Hop culture.";
+      const shortDescriptionUrl = shortDescription ? shortDescription.url : "";
+      const shortDescriptionDisplay = `<p>${shortDescription}</p>`;
+
+     
+  
+      // Update the HTML template to include a link to the product details page with the id of the selected product
+      html += `
+        <div class="product">
+          <h2>${product.name}</h2>
+          <a href="shop-details.html?id=${product.id}">
+            <img src="${imageUrl}" alt="${imageAlt}">
+          </a>
+          
+          <p>Price: ${product.prices.price}</p>
+          ${addToCartButton}
+        </div>
+      `;
+    });
+  
+    productContainer.innerHTML = html;
+  }
+  */
+
+  const url = "https://www.jagaranti.icu/wp-json/wc/store/products?_embed";
+
+async function getProducts() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getProducts().then(function(products) {
+  console.log(products);
+  displayProducts(products);
+});
+
+function displayProducts(products) {
+  const productContainer = document.querySelector(".product-container");
+  let html = "";
+
+  products.forEach(function(product) {
+    let imageUrl = "";
+    let imageAlt = "No image available";
+    if (product.images && product.images.length > 0) {
+      imageUrl = product.images[0].src;
+      imageAlt = product.name;
+    }
+
+    const addToCart = product.add_to_cart;
+    const addToCartText = addToCart ? addToCart.text : "Add to cart";
+    const addToCartUrl = addToCart ? addToCart.url : "";
+    const addToCartButton = `
+      <a href="${addToCartUrl}" class="add-to-cart">${addToCartText}</a>
+    `;
+
+    const shortDescription = product.short_description;
+    const shortDescriptionText = shortDescription ? shortDescription : "During the mid-1980s, the rap group NWA rises from the harsh streets of Compton, located in Los Angeles, California. Their music and lyrics, which depict life in the hood, lead to a revolution in Hip Hop culture.";
+    const shortDescriptionUrl = shortDescription ? shortDescription.url : "";
+    const shortDescriptionDisplay = `<p>${shortDescription}</p>`;
+
+    const currencyCode = product.prices.currency_code;
+    const currencySymbol = product.prices.currency_symbol;
+    const decimalSeparator = product.prices.currency_decimal_separator;
+    const thousandSeparator = product.prices.currency_thousand_separator;
+    const prefix = product.prices.currency_prefix;
+    const suffix = product.prices.currency_suffix;
+
+    const price = parseFloat(product.prices.price) / Math.pow(10, product.prices.currency_minor_unit);
+    const formattedPrice = prefix + price.toLocaleString(undefined, { minimumFractionDigits: product.prices.currency_minor_unit, maximumFractionDigits: product.prices.currency_minor_unit }) + suffix;
+    const priceDisplay = `<p>Price: ${formattedPrice}</p>`;
+
+    // Update the HTML template to include a link to the product details page with the id of the selected product
+    html += `
+      <div class="product">
+        <h2>${product.name}</h2>
+        <a href="shop-details.html?id=${product.id}">
+          <img src="${imageUrl}" alt="${imageAlt}">
+        </a>
+        
+        ${priceDisplay}
+        ${addToCartButton}
+      </div>
+    `;
+  });
+
+  productContainer.innerHTML = html;
+}
+
+  
+ 
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // this is for making the header stick to the top at all time
+window.onscroll = function () {
+  scrollPage();
+};
+
+const header = document.querySelector(".header");
+
+const sticky = header.offsetTop;
+
+function scrollPage() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// function for making the hamburger menu work
+function hamburgerMenu() {
+  var hamburgerLinks = document.querySelector(".links");
+  if (hamburgerLinks.style.display === "block") {
+    hamburgerLinks.style.display = "none";
+  } else {
+    hamburgerLinks.style.display = "block";
+  }
+}
+  
+
+  
+
+ 
+
+  
+
+  
+
+  
+ 
+  
+
+
+
+
+
+
+  
+
+
+
+ 
+
